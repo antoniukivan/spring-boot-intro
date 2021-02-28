c942f51deccc403d261b785d4f5336f5e292ef9d
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-28 21:34:50 +0200

    add RoleRepository interface to the repository package

--- src/main/java/com/example/boot/repository/RoleRepository.java
+++ src/main/java/com/example/boot/repository/RoleRepository.java
@@ -1,7 +1,11 @@
 package com.example.boot.repository;
 
 import com.example.boot.model.Role;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    default Optional<Role> getById(Long id) {
+        return Optional.of(getOne(id));
+    }
 }
--- src/main/java/com/example/boot/repository/UserRepository.java
+++ src/main/java/com/example/boot/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.boot.repository;
 
 import com.example.boot.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    default Optional<User> getById(Long id) {
+        return Optional.of(getOne(id));
+    }
 }
