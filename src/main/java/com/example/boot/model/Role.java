f43acc6d20ec5d929794f528344653fc00fa415a
antoniukivan <ivan.antonuk1992@gmail.com>
2021-02-28 22:19:43 +0200

    add RoleServiceImpl class to the service package

--- src/main/java/com/example/boot/model/Role.java
+++ src/main/java/com/example/boot/model/Role.java
@@ -18,7 +18,7 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "role_name")
+    @Column(name = "role_name", unique = true)
     @Enumerated(EnumType.STRING)
     private RoleName roleName;
 
---
+++ src/main/java/com/example/boot/service/impl/RoleServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.boot.service.impl;
+
+import com.example.boot.model.Role;
+import com.example.boot.repository.RoleRepository;
+import com.example.boot.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public Role save(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role getByName(String name) {
+        return roleRepository.getByName(name).get();
+    }
+}
