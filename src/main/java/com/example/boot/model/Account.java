c3ce8b47358e420de586135bd2a846fe2cba846f
antoniukivan <ivan.antonuk1992@gmail.com>
2021-03-02 12:15:35 +0200

    add Account class to the model package

---
+++ src/main/java/com/example/boot/model/Account.java
@@ -0,0 +1,30 @@
+package com.example.boot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "account_number", unique = true, nullable = false)
+    private String accountNumber;
+    private Currency currency;
+    private double balance;
+    private boolean isActive;
+    @ManyToOne
+    private User user;
+
+    public enum Currency {
+        USD, EUR, UAH
+    }
+}
