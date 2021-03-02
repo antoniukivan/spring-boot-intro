ce66e081f96d771c95b76d8b5f717baa24dc4ee3
antoniukivan <ivan.antonuk1992@gmail.com>
2021-03-02 13:39:30 +0200

    add TransactionRepository interface to the repository package

---
+++ src/main/java/com/example/boot/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.example.boot.repository;
+
+import com.example.boot.model.Account;
+import com.example.boot.model.Transaction;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("SELECT t FROM Transaction t "
+            + "WHERE t.accountFrom = ?1 OR t.accountTo = ?1 "
+            + "ORDER BY t.date DESC")
+    List<Transaction> findAllByAccount(Account account);
+}
